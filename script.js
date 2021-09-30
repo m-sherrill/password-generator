9e4d13f5ba5e4cc2f242aabed022299ee0c8572e
Morgan Sherrill <mo.sherrill@outlook.com>
2021-09-30 13:13:27 -0700

    started working on my code, forgot to start commiting. Got it half working but now trying to incorporate a look into the length prompt to make sure the user submits proper results

---
+++ script.js
@@ -0,0 +1,90 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// GeneratePassword function
+
+function generatePassword() {
+
+  var confirmNumber
+  var confirmChoices
+  var confirmUpper
+  var confirmLower
+  var passLength
+
+  // Arrays
+  number = [1, 2, 3, 4, 5, 6, 7, 8, 6]
+  lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "X"]
+
+
+    // parseInt, turns the answer into a number and passLength variable will be the user input
+  passLength = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"))
+    
+    while(true){ //loop runs until broken
+      if(!passLength) {
+          break
+          alert("Please enter a value");
+      } else (passLength < 8 || passLength > 128) { // number entered is <= 8 or >= 128
+          passLength = parseInt(prompt("Please select a number between 8 and 128"))
+      } 
+  }
+
+  confirmNumber = confirm("Will this contain numbers?")
+  confirmUpper = confirm("Will this contain Uppercase letters?")
+  confirmLower = confirm("Will this contain Lowercase letters?")
+        
+    if (!confirmNumber && !confirmUpper && !confirmLower) { // no options selected
+      alert("You must select at least one option")
+    } else if (confirmNumber && confirmUpper && confirmLower) { // all options selected
+      confirmChoices = number.concat(upper, lower)
+    } else if (confirmNumber && confirmLower) { // only numbers and lowercase selected
+      confirmChoices = number.concat(lower)
+    } else if (confirmNumber && confirmUpper) { // only number and uppercase select
+      confirmChoices = number.concat(upper)
+    } else if (confirmUpper && confirmLower) { // only uppercase and lowercase selected
+      confirmChoices = upper.concat(lower)
+    } else if (confirmNumber) { // only numbers selected
+      confirmChoices = number
+    } else if (confirmUpper) { // only uppercase selected
+      confirmChoices = upper
+    } else if (confirmLower) { // only lower case selected
+      confirmChoices = lower
+    }
+
+    // empty array for array manipulation 
+    var pw = []
+
+
+    for (var i = 0; i < passLength; i++) { // sets the length of the new array
+      var pickChoices = confirmChoices[Math.floor(Math.random() * confirmChoices.length)] // sets a new variable generating randomly from the confirmChoice
+      pw.push(pickChoices) // combines the coices from the prompts together
+    }
+
+
+    var password = pw.join("") // removes the comma between each character in the new array
+    return password // adds the final result
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword)
+
+// if(!passLength) { // no value entered
+    //   alert("Please enter a value")
+    // } else if (passLength < 8 || passLength > 128) { // number entered is <= 8 or >= 128
+    //   passLength = parseInt(prompt("Please select a number between 8 and 128"))
+    // } else if (passLength > 8 || passLength < 128) {
+    //     // after the user answers prompt, it continues to confirm what types of characters to add
+    //     confirmNumber = confirm("Will this contain numbers?")
+    //     confirmUpper = confirm("Will this contain Uppercase letters?")
+    //     confirmLower = confirm("Will this contain Lowercase letters?")
+    // } 
+      // parameters of choices
